1507d32f1f8bf429e8061d6aff2c1df0399d6e1d
yanosea <myanoshi0626@gmail.com>
2024-03-27 14:56:25 +0900

    ✨feat: add Partytown integration for Google Tag Manager

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -2,13 +2,25 @@ import { defineConfig } from "astro/config";
 import compress from "astro-compress";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 const FRONT_PORT = parseInt(process.env.FRONT_PORT , 10) || 4321;
 
 export default defineConfig({
-  integrations: [compress(), icon(), mdx(), sitemap(), tailwind()],
+  integrations: [
+    compress(),
+    icon(),
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    sitemap(),
+    tailwind(),
+  ],
   site: "https://yanosea.org",
   server: { port:FRONT_PORT , host: true },
   image: {
