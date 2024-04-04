f4f63066bfe42f885e02341fbd032087572965d1
yanosea <myanoshi0626@gmail.com>
2024-04-04 23:54:20 +0900

    ✨feat: implement light mode and theme toggle button

--- front/src/libs/blog.ts
+++ front/src/libs/blog.ts
@@ -2,9 +2,11 @@ import { getCollection } from "astro:content";
 import type { CollectionEntry } from "astro:content";
 
 export type Entries = CollectionEntry<"blog">[];
+
 export async function getAllEntries(): Promise<Entries> {
   return await getCollection("blog");
 }
+
 export function getAllEntryTags(entries: Entries): string[] {
   return [
     ...new Set(
@@ -12,6 +14,7 @@ export function getAllEntryTags(entries: Entries): string[] {
     ),
   ].sort();
 }
+
 export function sortEntriesByDateDesc(entries: Entries) {
   return entries.sort(
     (a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime(),
--- front/tailwind.config.cjs
+++ front/tailwind.config.cjs
@@ -1,16 +1,29 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         default: ["MaruMonica", "sans-serif"]
       },
       colors: {
-        main: "#D8DEE9",
-        sub: "#81A1C1",
-        sub_darker: "#5E81AC",
-        background: "#2E3440",
+        "main": "#3B4252",
+        "main-dark": "#D8DEE9",
+        "sub": "#81A1C1",
+        "sub-dark": "#81A1C1",
+        "sub-darker": "#5E81AC",
+        "sub-darker-dark": "#5E81AC",
+        "background": "#D8DEE9",
+        "background-dark": "#3B4252",
+        "red": "#BF616A",
+        "orange": "#D08770",
+        "green": "#A3BE8C",
+        "yellow": "#EBCB8B",
+        "yellow-lighter": "#FFE79D",
+        "purple": "#B48EAD",
+        "purple-lighter": "#C69ABE",
+        "blue": "#5E81AC",
       },
       typography: (theme) => ({
         DEFAULT: {
@@ -23,12 +36,37 @@ module.exports = {
             a: {
               color: theme("colors.main"),
               "&:hover": {
-                color: theme("colors.blue.600"),
+                color: theme("colors.blue"),
                 "transition-duration": "150ms",
               },
             },
             code: {
               color: theme("colors.sub"),
+            },
+            "code::before": {
+              content: "none",
+            },
+            "code::after": {
+              content: "none",
+            },
+          },
+        },
+        dark: {
+          css: {
+            maxWidth: "100%",
+            color: theme("colors.main-dark"),
+            "h1, h2, h3, h4, h5, h6, strong": {
+              color: theme("colors.main-dark"),
+            },
+            a: {
+              color: theme("colors.main-dark"),
+              "&:hover": {
+                color: theme("colors.blue"),
+                "transition-duration": "150ms",
+              },
+            },
+            code: {
+              color: theme("colors.sub-dark"),
             },
             "code::before": {
               content: "none",
