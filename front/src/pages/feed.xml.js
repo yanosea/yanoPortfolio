62e7d5bbe8cd5e4243ff04b58fa88561bcc9ea8b
yanosea <myanoshi0626@gmail.com>
2024-05-06 23:16:51 +0900

    ✨feat: fix the order of rss feed items (descendant)

--- front/src/pages/feed.xml.js
+++ front/src/pages/feed.xml.js
@@ -3,6 +3,9 @@ import { getCollection } from "astro:content";
 
 export async function GET(context) {
   const entries = await getCollection("blog");
+  entries.sort(
+    (a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime(),
+  );
   return rss({
     title: "yanosea.org blog",
     description: "blog rss feed of yanosea.org",
